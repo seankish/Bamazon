23d4559b4cac325b6a0cc84502d693148b665ea6
skish1987 <seankish1@gmail.com>
2019-07-23 22:29:40 -0700

    database created

--- bamazon.sql
+++ bamazon.sql
@@ -1,11 +1,13 @@
-create database bamazon;
-create table products (
-id INT not null auto_increment,
-productName varchar(45) null,
-departmentName varchar(45) null,
-price decimal(10,4)  null,
-stockQuanity int null,
-primary key (id),
-)
+DROP DATABASE IF EXISTS bamazon;
+CREATE DATABASE bamazon;
+use bamazon;
+CREATE TABLE products (
+  id INT NOT NULL AUTO_INCREMENT,
+  productName VARCHAR(45) NULL,
+  departmentName VARCHAR(45) NULL,
+  price decimal(10,4) null,
+  stockQuanity int null,
+  PRIMARY KEY (id)
+);
 
 
