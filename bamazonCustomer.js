2f0371f37bff9c2f85b8749b0f9a83b678a07830
skish1987 <seankish1@gmail.com>
2019-07-24 16:05:31 -0700

    l

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,8 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 // Load the NPM Package inquirer
-var mysql = require("mysql");
-var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -28,23 +26,46 @@ function start(){
         {
           type: "list",
           message: "What Item Would You Like to Buy?",
-          choices: ["Post an Item", "Bid on an Item"],
+          choices: [
+            "M240B Machine Gun", 
+            "MK1L 40mm Grenade Launcher",
+            "Seadoo GTI PRO Jetski",
+            "OuterLimits 50 SL SpeedBoat",
+            "Honda CRX450 Dirtbike",
+            "Ducati SuperSport 900 Motorcycle",
+            "Ferrari Enzo Supercar",
+            "Bugatti Veyron Supercar",
+            "Lamborghini sesto Elemento Supercar",
+            "Gian Inflatable Unicorn"],
           name: "item-list"
         },
       
      
       ])
-      .then(function(inquirerResponse) {
-        if(inquirerResponse.post-bid ==="Post an Item"){
-          console.log("You're  A SELLER")
-          postAuction();
+      .then(function(answer) {
+        switch (answer.action) {
+        case "Find songs by artist":
+          artistSearch();
+          break;
+  
+        case "Find all artists who appear more than once":
+          multiSearch();
+          break;
+  
+        case "Find data within a specific range":
+          rangeSearch();
+          break;
+  
+        case "Search for a specific song":
+          songSearch();
+          break;
+            
+        case "exit":
+          connection.end();
+          break;
         }
-        else if(inquirerResponse.post-bid==="Bid on an Item"){
-          console.log("You're a BUYER!");
-        }
-        else{
-            connection.end();
-        }
+      });
+  }
         // // If the inquirerResponse confirms, we displays the inquirerResponse's username and pokemon from the answers.
         // if (inquirerResponse.bear =="polar bear") {
         //   console.log("\nWelcome " + inquirerResponse.username);
